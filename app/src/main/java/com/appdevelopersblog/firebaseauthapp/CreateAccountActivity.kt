5ee75a5dbce03e37f2b48e2bf86cbfc6b2c7f50f
Shubham <shubham.aggarwal@concirrus.com>
2017-10-23 22:56:57 +0530

    Updated code for using firebase Storage to allow user to upload profile pictres as well

--- app/src/main/java/com/appdevelopersblog/firebaseauthapp/CreateAccountActivity.kt
+++ app/src/main/java/com/appdevelopersblog/firebaseauthapp/CreateAccountActivity.kt
@@ -1,7 +1,9 @@
 package com.appdevelopersblog.firebaseauthapp
 
+import android.app.Activity
 import android.app.ProgressDialog
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
@@ -9,10 +11,15 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 
 class CreateAccountActivity : AppCompatActivity() {
 
@@ -32,10 +39,17 @@ class CreateAccountActivity : AppCompatActivity() {
     private var btnCreateAccount: Button? = null
     private var mProgressBar: ProgressDialog? = null
 
+    //profile pic references
+    private var profilePic: ImageButton? = null
+    private var resultUri: Uri? = null
+    private val GALLERY_CODE = 1
+    private val IMAGE_KEY = "user_profile_picture"
+
     //Firebase references
     private var mDatabaseReference: DatabaseReference? = null
     private var mDatabase: FirebaseDatabase? = null
     private var mAuth: FirebaseAuth? = null
+    private var mFirebaseStorage: StorageReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,13 +65,24 @@ class CreateAccountActivity : AppCompatActivity() {
         etEmail = findViewById<View>(R.id.et_email) as EditText
         etPassword = findViewById<View>(R.id.et_password) as EditText
         btnCreateAccount = findViewById<View>(R.id.btn_register) as Button
+        profilePic = findViewById<View>(R.id.ib_profile_pic) as ImageButton
         mProgressBar = ProgressDialog(this)
 
         mDatabase = FirebaseDatabase.getInstance()
         mDatabaseReference = mDatabase!!.reference!!.child("Users")
         mAuth = FirebaseAuth.getInstance()
+        mFirebaseStorage = FirebaseStorage.getInstance().reference.child(IMAGE_KEY)
 
         btnCreateAccount!!.setOnClickListener { createNewAccount() }
+
+        profilePic!!.setOnClickListener { pickImageFromGallery() }
+    }
+
+    private fun pickImageFromGallery() {
+        val galleryIntent = Intent()
+        galleryIntent.action = Intent.ACTION_GET_CONTENT
+        galleryIntent.type = "image/*"
+        startActivityForResult(galleryIntent, GALLERY_CODE)
     }
 
     private fun createNewAccount() {
@@ -76,21 +101,25 @@ class CreateAccountActivity : AppCompatActivity() {
             mAuth!!.createUserWithEmailAndPassword(email!!, password!!)
                     .addOnCompleteListener(this) { task ->
 
-                        mProgressBar!!.hide()
-
                         if (task.isSuccessful) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success")
 
                             val userId = mAuth!!.currentUser!!.uid
 
+                            verifyEmail()
+
                             //update user profile information
                             val currentUserDb = mDatabaseReference!!.child(userId)
                             currentUserDb.child("firstName").setValue(firstName)
                             currentUserDb.child("lastName").setValue(lastName)
 
+                            uploadImage()
+                            mProgressBar!!.hide()
+
                             updateUserInfoAndUI()
                         } else {
+                            mProgressBar!!.hide()
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.exception)
                             Toast.makeText(this@CreateAccountActivity, "Authentication failed.",
@@ -103,6 +132,36 @@ class CreateAccountActivity : AppCompatActivity() {
         }
     }
 
+    private fun uploadImage() {
+
+        val imagePath = mFirebaseStorage!!.child(IMAGE_KEY).child(resultUri!!.lastPathSegment)
+
+        imagePath.putFile(resultUri!!).addOnSuccessListener { taskSnapshot ->
+            mDatabaseReference!!
+                    .child(mAuth!!.currentUser!!.uid)
+                    .child("image")
+                    .setValue(taskSnapshot.downloadUrl)
+        }
+    }
+
+    private fun verifyEmail() {
+        val mUser = mAuth!!.currentUser;
+        mUser!!.sendEmailVerification()
+                .addOnCompleteListener(this) { task ->
+
+                    if (task.isSuccessful) {
+                        Toast.makeText(this@CreateAccountActivity,
+                                "Verification email sent to " + mUser.getEmail(),
+                                Toast.LENGTH_SHORT).show()
+                    } else {
+                        Log.e(TAG, "sendEmailVerification", task.exception)
+                        Toast.makeText(this@CreateAccountActivity,
+                                "Failed to send verification email.",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                }
+    }
+
     private fun updateUserInfoAndUI() {
 
         //start next activity
@@ -110,4 +169,30 @@ class CreateAccountActivity : AppCompatActivity() {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         startActivity(intent)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GALLERY_CODE && resultCode == Activity.RESULT_OK) {
+            val mImageUri = data.data
+
+            CropImage.activity(mImageUri)
+                    .setAspectRatio(1, 1)
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .start(this)
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            val result = CropImage.getActivityResult(data)
+            if (resultCode == Activity.RESULT_OK) {
+                resultUri = result.getUri()
+
+                profilePic!!.setImageURI(resultUri)
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                val error = result.getError()
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/appdevelopersblog/firebaseauthapp/MainActivity.kt
+++ app/src/main/java/com/appdevelopersblog/firebaseauthapp/MainActivity.kt
@@ -3,10 +3,11 @@ package com.appdevelopersblog.firebaseauthapp
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.ImageButton
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
-
+import com.squareup.picasso.Picasso
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,8 @@ class MainActivity : AppCompatActivity() {
     private var tvFirstName: TextView? = null
     private var tvLastName: TextView? = null
     private var tvEmail: TextView? = null
+    private var tvEmailVerifiied: TextView? = null
+    private var profilePic: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +39,8 @@ class MainActivity : AppCompatActivity() {
         tvFirstName = findViewById<View>(R.id.tv_first_name) as TextView
         tvLastName = findViewById<View>(R.id.tv_last_name) as TextView
         tvEmail = findViewById<View>(R.id.tv_email) as TextView
+        tvEmailVerifiied = findViewById<View>(R.id.tv_email_verifiied) as TextView
+        profilePic = findViewById<View>(R.id.ib_profile_pic) as ImageButton
     }
 
     override fun onStart() {
@@ -45,11 +50,16 @@ class MainActivity : AppCompatActivity() {
         val mUserReference = mDatabaseReference!!.child(mUser!!.uid)
 
         tvEmail!!.text = mUser.email
+        tvEmailVerifiied!!.text = mUser.isEmailVerified.toString()
 
         mUserReference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 tvFirstName!!.text = snapshot.child("firstName").value as String
                 tvLastName!!.text = snapshot.child("lastName").value as String
+
+                Picasso.with(this@MainActivity)
+                        .load(snapshot.child("image").value as String)
+                        .into(profilePic)
             }
 
             override fun onCancelled(databaseError: DatabaseError) {}
